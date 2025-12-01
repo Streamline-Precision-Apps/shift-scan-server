e81e4a0414767c8f80fb266ca120fc4439471a5e
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-12-01 14:21:34 -0700

    save certificate and capactitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.shiftscanapp",
   appName: "Shift Scan",
-  webDir: "out",
+  webDir: "client/out",
 
   // server: {
   //   url: "http://192.168.1.102:3000",
