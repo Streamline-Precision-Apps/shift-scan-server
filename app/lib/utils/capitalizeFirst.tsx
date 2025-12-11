365a5fa406653c3048ee396045f97199e504a365
Streamline Apps <122407589+devunfox15@users.noreply.github.com>
2025-12-11 15:07:21 -0700

    removed all unnecessary files from server

--- app/lib/utils/capitalizeFirst.tsx
+++
@@ -1,6 +0,0 @@
-export default function Capitalize(str: string) {
-  if (typeof str !== "string") {
-    return "";
-  }
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
--- app/lib/utils/getRawPhoneNumber.ts
+++
@@ -1,2 +0,0 @@
-export const getRawPhoneNumber = (formatted: string) =>
-  formatted.replace(/\D/g, "");
